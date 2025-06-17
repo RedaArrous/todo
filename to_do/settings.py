3c48f40136428f0c1d0052794756f81d9637b410
reda <hissocka2006@gmail.com>
2025-06-17 23:57:50 +0100

    first commit

--- to_do/settings.py
+++ to_do/settings.py
@@ -74,16 +74,25 @@ WSGI_APPLICATION = 'to_do.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todo',
-        'USER': 'reda',
-        'PASSWORD': 'mamabentkada @@',
-        'HOST': 'localhost',
-        'PORT': '5433',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'todo',
+#         'USER': 'reda',
+#         'PASSWORD': 'mamabentkada @@',
+#         'HOST': 'localhost',
+#         'PORT': '5433',
+#     }
+# }
 
 
 # Password validation
@@ -134,7 +143,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'hissocka2006@gmail.com'
-EMAIL_HOST_PASSWORD = "pytikhzoidrzcsxf"
+EMAIL_HOST_PASSWORD = ""
 DEFAULT_FROM_EMAIL = 'hissocka2006@gmail.com'
 
 
